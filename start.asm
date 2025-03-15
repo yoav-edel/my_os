384e8c10488a886792f16c46989692a18134be4f
Yoav Luria Edelstein <yoavedel123@gmail.com>
2025-03-15 13:30:48 +0200

    Don't know how to describe what I did

--- start.asm
+++ start.asm
@@ -1,17 +1,16 @@
 section .text
-    global start
-    extern kernel_main
+global start
+global _kernel_stack_top
+global _kernel_stack_pages_amount
+
+extern kernel_main
+
+_kernel_stack_top dd 0x4000000 - 1
+
+_kernel_stack_pages_amount dd 100
 
 start:
-    cli                         ; Clear interrupts
-    mov esp, [_kernel_stack_top]  ; Set stack pointer
-    call kernel_main            ; Jump to kernel main function
-    hlt                         ; Halt CPU
-
-
-section .data
-    global _kernel_stack_top
-    _kernel_stack_top: dd 0x1000000 - 1
-
-    global _kernel_stack_pages_amount
-    _kernel_stack_pages_amount: dd 30
+    cli                          ; Clear interrupts
+    mov esp, [_kernel_stack_top] ; Now loads 0x3FFFFFF directly into ESP
+    call kernel_main             ; Jump to kernel main function
+    hlt                          ; Halt CPU
