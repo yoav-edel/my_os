89b40f9603d8ee3ca762fa3016cd30a130c4c6db
Yoav Luria Edelstein <yoavedel123@gmail.com>
2025-03-20 19:08:40 +0200

    Added more functionality to the pcb and better architecture

--- processes/pcb.c
+++ processes/pcb.c
@@ -3,15 +3,37 @@
 //
 
 #include "pcb.h"
+#include "kmalloc.h"
 
-pcb_t *create_pcb(context_t context, page_directory_t *page_dir) {
+context_t *context_create(uint32_t eip, uint32_t esp) {
+    context_t *context = kmalloc(sizeof(context_t));
+    context->edi = 0;
+    context->esi = 0;
+    context->ebp = 0;
+    context->esp = esp;
+    context->ebx = 0;
+    context->edx = 0;
+    context->ecx = 0;
+    context->eax = 0;
+    context->eip = eip;
+    context->eflags = DEFAULT_EFLAGS;
+    return context;
+}
+
+void context_destroy(context_t *context) {
+    kfree(context);
+}
+
+pcb_t *pcb_create(uint32_t eip, uint32_t esp, page_directory_t *page_dir) {
     pcb_t *pcb = kmalloc(sizeof(pcb_t));
-    pcb->context = context;
+    pcb->context = context_create(eip, esp);
     pcb->page_dir = page_dir;
     pcb->state = READY;
     return pcb;
 }
 
-void destroy_pcb(pcb_t *pcb) {
+void pcb_destroy(pcb_t *pcb) {
+    if(pcb == NULL) return;
+    context_destroy(pcb->context);
     kfree(pcb);
 }
--- processes/pcb.h
+++ processes/pcb.h
@@ -7,9 +7,10 @@
 #ifndef MYKERNEL_PCB_H
 #define MYKERNEL_PCB_H
 
+#define DEFAULT_EFLAGS 0x202 // the 1-bit is always 1, and the 9th bit enables interrupts
 
 //todo add support of more regiristers
-struct {
+typedef struct {
     uint32_t edi;
     uint32_t esi;
     uint32_t ebp;
@@ -22,20 +23,18 @@ struct {
     uint32_t eflags;
 } context_t;
 
-typedef uint32_t pid_t;
+context_t *context_create(uint32_t eip, uint32_t esp);
+void context_destroy(context_t *context);
 
 typedef struct pcb {
-    context_t context;
+    context_t *context;
     page_directory_t *page_dir;
     process_state_t state;
 } pcb_t;
 
-pcb_t *create_pcb(context_t
-context,
-page_directory_t *page_dir
-);
+pcb_t *pcb_create(uint32_t eip, uint32_t esp, page_directory_t *page_dir);
 
-void destroy_pcb(pcb_t *pcb);
+void pcb_destroy(pcb_t *pcb);
 
 
 #endif //MYKERNEL_PCB_H
